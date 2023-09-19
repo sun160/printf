86736cf99162d1e23baede05fbbaf5307033fbd3
mohamedbaker <dam.vampire22@gmail.com>
2023-09-19 12:48:43 -0700

    main func that has all protypes

---
+++ main.h
@@ -0,0 +1,28 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdarg.h>
+#include <stdlib.h>
+
+
+/**
+ * struct formatSelect - function to check for formats
+ * @ptr: specifier type
+ * @fun: The print function to use
+*/
+
+typedef struct formatSelect
+{
+	char *ptr;
+	int (*fun)();
+} formatSelect;
+
+int _printf(const char *format, ...);
+int _putchar(char c);
+int print_char(va_list args);
+int print_string(va_list args);
+int convert_int(va_list args);
+
+#endif
